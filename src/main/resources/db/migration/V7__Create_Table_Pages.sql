a159f72f3f10ea915b47b9fa23c88e9094c2c86c
Leonardo Silva <s.leosoares@gmail.com>
2023-05-15 00:48:09 -0300

    feat: created migrations for pages tables

---
+++ src/main/resources/db/migration/V7__Create_Table_Pages.sql
@@ -0,0 +1,37 @@
+CREATE TABLE IF NOT EXISTS `pages` (
+  `id` BIGINT(20) NOT NULL AUTO_INCREMENT,
+  `title` VARCHAR(255) NOT NULL,
+  `summary` VARCHAR(255),
+  `body` LONGTEXT,
+  `status` VARCHAR(100) NOT NULL,
+  `created_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `deleted_at` DATETIME,
+  `user_id` BIGINT(20) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `id_UNIQUE` (`id`),
+  INDEX `user_id_idx` (`user_id`),
+  CONSTRAINT `fk_pages_users_user_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+);
+
+CREATE TABLE IF NOT EXISTS `pages_tags` (
+  `page_id` BIGINT(20),
+  `tag_id` BIGINT(20),
+  PRIMARY KEY (`page_id`, `tag_id`),
+  UNIQUE INDEX `page_id_tag_id_UNIQUE` (`page_id`, `tag_id`),
+  INDEX `page_id_idx` (`page_id`),
+  INDEX `tag_id_idx` (`tag_id`),
+  CONSTRAINT `fk_pages_tags_page_id` FOREIGN KEY (`page_id`) REFERENCES `pages` (`id`),
+  CONSTRAINT `fk_pages_tags_tag_id` FOREIGN KEY (`tag_id`) REFERENCES `tags` (`id`)
+);
+
+CREATE TABLE IF NOT EXISTS `pages_comments` (
+  `page_id` BIGINT(20),
+  `comment_id` BIGINT(20),
+  PRIMARY KEY (`page_id`, `comment_id`),
+  UNIQUE INDEX `page_id_tag_id_UNIQUE` (`page_id`, `comment_id`),
+  INDEX `page_id_idx` (`page_id`),
+  INDEX `comment_id_idx` (`comment_id`),
+  CONSTRAINT `fk_pages_comments_page_id` FOREIGN KEY (`page_id`) REFERENCES `pages` (`id`),
+  CONSTRAINT `fk_pages_comments_comment_id` FOREIGN KEY (`comment_id`) REFERENCES `comments` (`id`)
+);
---
+++ src/main/resources/db/migration/V8__Populate_Table_Pages.sql
@@ -0,0 +1,22 @@
+-- Lorem Ipsum
+-- https://www.lipsum.com/
+
+INSERT INTO `pages` (`id`, `title`, `summary`, `body`, `status`, `user_id`) VALUES
+    (1, 'Nam gravida sem in nunc.', 'Phasellus lorem tellus, auctor et vestibulum nec, ornare at turpis.', 'Duis ut consectetur purus. Ut consequat elementum lacinia. Sed ornare elit eu est vulputate pretium. Donec non pretium nulla. Suspendisse suscipit tristique fringilla. Curabitur odio.', 'published', 1),
+    (2, 'Suspendisse potenti. Duis sit amet.', 'Sed eget ipsum fermentum, blandit erat ac, pretium tellus. Vestibulum.', 'Etiam at nisl lectus. Pellentesque aliquet nec diam at consequat. Ut consequat metus et diam eleifend, eget pretium purus suscipit. Aenean lacinia porta metus, quis.', 'published', 2),
+    (3, 'In rhoncus finibus velit in.', null, 'Vestibulum mattis odio ac nisl imperdiet facilisis. Quisque vitae euismod odio. Vivamus dapibus vitae leo at eleifend. Morbi pharetra, velit ac laoreet lobortis, lectus felis.', 'hidden', 1),
+    (4, 'Duis iaculis volutpat ipsum in.', 'Aliquam gravida non mauris vitae imperdiet. Nulla in ultrices augue.', null, 'draft', 1);
+
+INSERT INTO `pages_tags` (`page_id`, `tag_id`) VALUES
+    (1, 1),
+    (1, 2),
+    (1, 3),
+    (2, 2),
+    (3, 5);
+
+INSERT INTO `pages_comments` (`page_id`, `comment_id`) VALUES
+    (1, 1),
+    (1, 3),
+    (1, 5),
+    (2, 4),
+    (3, 8);
